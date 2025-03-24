4a8a179c5d0a1db7a88d94d3d4ee08f094e4cf7f
Furkan Gemici <furkngemiic@gmail.com>
2025-03-24 23:40:16 +0300

    Basic Calculater Added

---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace HesapMakinesi
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, toplam;
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+            toplam = sayi1 + sayi2;
+            label6.Text = toplam.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = Convert.ToInt32(textBox4.Text);
+            b = Convert.ToInt32(textBox3.Text);
+            c = a * b;
+            label7.Text = c.ToString();
+
+        }
+    }
+}
